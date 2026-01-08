d44777e873daf524d32c0d260c55cb9362b1f039
BlackRock <denis747474@mail.ru>
2026-01-08 21:46:20 +0300

    fix: shim @react-native-async-storage for MetaMask SDK on Vercel

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
+  },
+  webpack: (config) => {
+    config.resolve.alias['@react-native-async-storage/async-storage'] = require.resolve('./src/shim-async-storage.js');
+    return config;
   },
 };
 
